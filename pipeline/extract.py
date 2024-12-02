30dc4539cc3d7c83e531e7c3296a33d7860d1d1e
Jakub-Poskrop <jakubposkrop05@gmail.com>
2024-12-02 15:31:46 +0000

    Added function docstring

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -6,6 +6,10 @@ URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojso
 
 
 def get_data() -> list[dict]:
+    '''
+    Function to get the necessary data from the API. 
+    Only gets data that was uploaded within the last minute as the pipeline will run every minute
+    '''
     data = []
     response = requests.get(URL)
     earthquakes_data = response.json()["features"]
@@ -13,7 +17,8 @@ def get_data() -> list[dict]:
     for earthquake_data in earthquakes_data:
         time_uploaded_at = datetime.fromtimestamp(
             earthquake_data["properties"]["updated"]/1000)
-        if time_uploaded_at.hour != current_time.hour or time_uploaded_at.minute < current_time.minute - 1:
+        if (time_uploaded_at.hour != current_time.hour or
+                time_uploaded_at.minute < current_time.minute - 1):
             continue
         if earthquake_data.get("properties"):
             data.append({
