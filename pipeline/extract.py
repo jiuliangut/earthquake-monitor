8f5ecc1712c34329a19d168d25c7d54c84350f0e
Jakub-Poskrop <jakubposkrop05@gmail.com>
2024-12-02 15:25:34 +0000

    Improved extract

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,5 +1,7 @@
 '''Module that queries all the earthquake API and stores all of the wanted data'''
 import requests
+from datetime import datetime, timedelta
+import time
 
 URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
 
@@ -8,10 +10,15 @@ def get_data() -> list[dict]:
     data = []
     response = requests.get(URL)
     earthquakes_data = response.json()["features"]
+    current_time = datetime.now()
     for earthquake_data in earthquakes_data:
+        time_uploaded_at = datetime.fromtimestamp(
+            earthquake_data["properties"]["updated"]/1000)
+        if time_uploaded_at.hour != current_time.hour or time_uploaded_at.minute < current_time.minute - 1:
+            continue
         if earthquake_data.get("properties"):
             data.append({
-                "at": earthquake_data["properties"]["time"],
+                "at": datetime.fromtimestamp(earthquake_data["properties"]["time"]/1000),
                 "event_url": earthquake_data["properties"]["detail"],
                 "felt": earthquake_data["properties"]["felt"],
                 "location": earthquake_data["properties"]["place"],
