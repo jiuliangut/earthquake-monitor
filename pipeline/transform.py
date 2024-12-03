ceda96819d2c20bc6f5b34e4fabdf73109426851
qrafiq1 <qrafiq1233@gmail.com>
2024-12-03 11:57:12 +0000

    Added validation for key readings and logging

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,23 +1,119 @@
+# pylint: disable=line-too-long
+
 '''
 Module that transforms the extracted data into the necessary format. 
 Ready to be loaded into the database
 '''
 import datetime
+import logging
 import pandas as pd
 
+MAX_MAGNITUDE = 12.0
+MIN_MAGNITUDE = -10.0
+MAX_LON = 180.0
+MIN_LON = -180.0
+MAX_LAT = 90.0
+MIN_LAT = -90.0
+MAX_CDI = 12.0
+MIN_CDI = 0.0
 
-def clean_data(earthquake_data: list[dict]) -> pd.DataFrame:
+GREEN = "green"
+ZERO = 0
+
+
+def is_valid_latitude(lat: float) -> bool:
+    """Checks if latitude is valid"""
+    if not isinstance(lat, (float, int)):
+        logging.error("Invalid latitude type: %s", type(lat))
+        return False
+    return MIN_LAT <= lat <= MAX_LAT
+
+
+def is_valid_longitude(lon: float) -> bool:
+    """Checks if longitude is valid"""
+    if not isinstance(lon, (float, int)):
+        logging.error("Invalid longitude type: %s", type(lon))
+        return False
+    return MIN_LON <= lon <= MAX_LON
+
+
+def is_valid_magnitude(mag: float) -> bool:
+    """Checks if magnitude is valid"""
+    if not isinstance(mag, (float, int)):
+        logging.error("Invalid magnitude type: %s", type(mag))
+        return False
+    return MIN_MAGNITUDE <= mag <= MAX_MAGNITUDE
+
+
+def is_valid_cdi(cdi: float) -> bool:
+    """Checks if cdi is valid"""
+    if not isinstance(cdi, (float, int)):
+        logging.error("Invalid CDI type: %s", type(cdi))
+        return False
+    return MIN_CDI <= cdi <= MAX_CDI
+
+
+def clean_data(earthquake_data: list[dict]) -> list[dict]:
     '''
-    Function to clean the data extracted
-    Replaces any None values with actual values 
+    Function to clean the data extracted from the earthquake API.
+    - Converts 'felt' and 'cdi' columns to numeric values, replacing invalid or missing values with defaults.
+    - Replaces missing values in the 'alert' column with 'green' as the default value.
+    - Gets rid of milliseconds in timestamp.
     '''
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
+    if not isinstance(earthquake_data, list):
+        logging.error("Expected list for earthquake_data, got %s",
+                      type(earthquake_data))
+        return []
+
+    logging.info("Cleaning earthquake data...")
     earthquake_df = pd.DataFrame(earthquake_data)
+
     earthquake_df["felt"] = pd.to_numeric(
-        earthquake_df["felt"], errors="coerce").fillna(0)
-    earthquake_df["alert"] = earthquake_df["alert"].fillna("green")
+        earthquake_df["felt"], errors="coerce").fillna(ZERO)
+    earthquake_df["alert"] = earthquake_df["alert"].fillna(GREEN)
     earthquake_df["cdi"] = pd.to_numeric(
-        earthquake_df["cdi"], errors="coerce").fillna(-1)
-    return earthquake_df
+        earthquake_df["cdi"], errors="coerce").fillna(MIN_CDI)
+
+    earthquake_df = validate_data(earthquake_df)
+
+    earthquake_df["at"] = earthquake_df['at'].apply(
+        lambda x: x.replace(microsecond=ZERO))
+
+    logging.info("Data cleaning complete.")
+    return earthquake_df.to_dict(orient='records')
+
+
+def validate_data(earthquake_df: pd.DataFrame) -> pd.DataFrame:
+    """Ensures that long, lat, magnitude, cdi readings are within a valid range"""
+    logging.info("Validating data...")
+
+    earthquake_df["latitude_valid"] = earthquake_df["latitude"].apply(
+        is_valid_latitude)
+    earthquake_df["longitude_valid"] = earthquake_df["longitude"].apply(
+        is_valid_longitude)
+    earthquake_df["magnitude_valid"] = earthquake_df["magnitude"].apply(
+        is_valid_magnitude)
+    earthquake_df["cdi_valid"] = earthquake_df["cdi"].apply(is_valid_cdi)
+
+    valid_earthquake_df = earthquake_df[earthquake_df["latitude_valid"] &
+                                        earthquake_df["longitude_valid"] &
+                                        earthquake_df["magnitude_valid"] &
+                                        earthquake_df["cdi_valid"]]
+
+    invalid_count = len(earthquake_df) - len(valid_earthquake_df)
+    if invalid_count > 0:
+        logging.warning(
+            "Removed %s invalid earthquake records.", invalid_count)
+
+    columns_to_exclude = ["latitude_valid",
+                          "longitude_valid", "magnitude_valid", "cdi_valid"]
+    valid_earthquake_df.drop(columns=columns_to_exclude, inplace=True)
+
+    logging.info("Data validation complete.")
+    return valid_earthquake_df
 
 
 if __name__ == "__main__":
