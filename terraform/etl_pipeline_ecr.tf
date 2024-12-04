953de5333c22a6d59579813fca6bbc09c4b1d9fc
qrafiq1 <qrafiq1233@gmail.com>
2024-12-04 10:50:01 +0000

    Uncommented

--- terraform/etl_pipeline_ecr.tf
+++ terraform/etl_pipeline_ecr.tf
@@ -1,31 +1,31 @@
-# resource "aws_ecr_repository" "c14_earthquake_monitor_etl_ecr" {
-#   name                 = "c14_earthquake_monitor_etl_ecr"
-#   image_tag_mutability = "MUTABLE"
+resource "aws_ecr_repository" "c14_earthquake_monitor_etl_ecr" {
+  name                 = "c14_earthquake_monitor_etl_ecr"
+  image_tag_mutability = "MUTABLE"
 
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
-# }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
 
-# resource "aws_ecr_lifecycle_policy" "c14_earthquake_monitor_etl_ecr_lifecycle_policy" {
-#   repository = aws_ecr_repository.c14_earthquake_monitor_etl_ecr.name
+resource "aws_ecr_lifecycle_policy" "c14_earthquake_monitor_etl_ecr_lifecycle_policy" {
+  repository = aws_ecr_repository.c14_earthquake_monitor_etl_ecr.name
 
-#   policy = <<EOF
-# {
-#     "rules": [
-#         {
-#             "rulePriority": 1,
-#             "description": "Keep last 30 images",
-#             "selection": {
-#                 "tagStatus": "tagged",
-#                 "countType": "imageCountMoreThan",
-#                 "countNumber": 30
-#             },
-#             "action": {
-#                 "type": "expire"
-#             }
-#         }
-#     ]
-# }
-# EOF
-# }
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 30 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
