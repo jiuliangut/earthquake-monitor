5ffd2f8e6423a3e17970c6e1bac454b289253992
jiuliangut <jiuliancarlo@gmail.com>
2024-12-04 17:29:39 +0000

    implemented pd.read_sql

--- data_upload/extract.py
+++ data_upload/extract.py
@@ -6,29 +6,23 @@ from dotenv import load_dotenv
 import boto3
 import psycopg2
 import psycopg2.extras
+import pandas as pd
 from psycopg2.extensions import connection, cursor
 
-QUERY = """ SELECT
-    e.earthquake_id,
-    e.time,
-    e.magnitude,
-    e.tsunami,
-    e.felt_report_count,
-    e.cdi,
-    e.latitude,
-    e.longitude,
-    e.depth,
-    e.detail_url,
-    a.alert_type,
-    m.magnitude_type,
-    n.network_name,
-    t.type_name
-    FROM earthquakes AS e
-    JOIN alerts AS a ON e.alert_id = a.alert_id
-    JOIN networks AS n ON e.network_id = n.network_id
-    JOIN magnitude AS m ON e.magnitude_id = m.magnitude_id
-    JOIN type AS t ON e.type_id = t.type_id;
+QUERY = """ 
+SELECT *
+FROM earthquakes AS e
+JOIN alerts AS a ON e.alert_id = a.alert_id
+JOIN networks AS n ON e.network_id = n.network_id
+JOIN magnitude AS m ON e.magnitude_id = m.magnitude_id
+JOIN type AS t ON e.type_id = t.type_id;
 """
+
+COLUMNS = [
+    'earthquake_id', 'time', 'magnitude', 'tsunami', 'felt_report_count', 'cdi',
+    'latitude', 'longitude', 'depth', 'detail_url', 'alert_type', 'magnitude_type',
+    'network_name', 'type_name']
+
 
 CSV_FILE = f"/tmp/{datetime.today().strftime('%Y-%m-%d')}_data.csv"
 
@@ -44,27 +38,15 @@ def get_connection() -> connection:
     )
 
 
-def get_cursor(conn: connection) -> cursor:
-    """Returns cursor for the connection"""
-    return conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
-
 def extract_data() -> None:
     """Extracts data from database and writes to a CSV file"""
     conn = None
     try:
         conn = get_connection()
-        cur = get_cursor(conn)
         print("Executing query...")
-        cur.execute(QUERY)
-        results = cur.fetchall()
-        column_titles = [desc[0] for desc in cur.description]
-
-        with open(CSV_FILE, mode="w", newline="", encoding="utf-8") as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerow(column_titles)
-            writer.writerows([list(row.values()) for row in results])
-
+        earthquakes = pd.read_sql(QUERY, conn)
+        earthquakes = earthquakes[COLUMNS]
+        earthquakes.to_csv(CSV_FILE, index=False, encoding="utf-8")
         print(f"Data successfully written to {CSV_FILE}")
     except Exception as e:
         print(f"Error: {e}")
@@ -84,14 +66,14 @@ def get_client():
 
 
 def upload_to_s3():
-    """Uploads CSV file to S3 bucket"""
+    """Uploads CSV file to S3 bucket and clears temp file"""
     s3_client = get_client()
     bucket_name = os.getenv("BUCKET_NAME")
     s3_key = os.path.basename(CSV_FILE)
-
     try:
         print(f"Uploading {CSV_FILE} to bucket...")
         s3_client.upload_file(CSV_FILE, bucket_name, s3_key)
+        os.remove(CSV_FILE)
         print("Upload successful.")
     except Exception as e:
         print(f"Failed to upload file to S3: {e}")
