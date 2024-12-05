32f408ca7b8ac8d51aa50c9fb4fa22fc536169dc
qrafiq1 <qrafiq1233@gmail.com>
2024-12-05 21:42:32 +0000

    Finished UI for subscription

--- dashboard/pages/subscribe.py
+++ dashboard/pages/subscribe.py
@@ -36,61 +36,90 @@ def setup_page():
         st.write("")
 
     with middle:
+        if "form_data" not in st.session_state:
+            st.session_state.form_data = {
+                "first_name": "",
+                "last_name": "",
+                "email": "",
+                "phone": "",
+                "regions": [],
+                "min_magnitude": None,
+            }
+
         contact_preference = st.radio(
             "How would you like to be contacted?",
             options=["Email", "Phone", "Both"]
         )
-        with st.form(key="subscribe_form", clear_on_submit=True):
+
+        form_data = st.session_state.form_data
+
+        with st.form(key="subscribe_form", clear_on_submit=False):
             st.write("Subscription Form")
-            first_name = st.text_input("First Name")
-            last_name = st.text_input("Last Name")
+            first_name = st.text_input(
+                "First Name", value=form_data["first_name"])
+            last_name = st.text_input(
+                "Last Name", value=form_data["last_name"])
 
             email = None
             phone = None
 
             if contact_preference == "Email" or contact_preference == "Both":
-                email = st.text_input("Email", placeholder="example@gmail.com")
+                email = st.text_input(
+                    "Email", placeholder="example@gmail.com", value=form_data["email"]
+                )
 
             if contact_preference == "Phone" or contact_preference == "Both":
                 phone = st.text_input(
-                    "Phone Number", placeholder="07123456789")
+                    "Phone Number", placeholder="07123456789", value=form_data["phone"]
+                )
 
             regions = st.multiselect(
-                "Regions", options=test, placeholder="Choose a region")
+                "Regions", options=test, placeholder="Choose a region", default=form_data["regions"]
+            )
 
             option_map = {
                 0: "All Earthquakes",
                 4: "Noticeable Earthquakes (4.0+)",
-                7: "Strong Earthquakes (7.0+)"
+                7: "Strong Earthquakes (7.0+)",
             }
 
-            min_magnitude = st.pills("Minimum magnitude", options=option_map.keys(
-            ), format_func=lambda option: option_map[option])
-
+            min_magnitude = st.pills(
+                "Minimum magnitude",
+                options=option_map.keys(),
+                format_func=lambda option: option_map[option],
+                key="min_magnitude",
+            )
             subscribe_button = st.form_submit_button("Subscribe")
 
             if subscribe_button:
+                form_data["first_name"] = first_name
+                form_data["last_name"] = last_name
+                form_data["email"] = email
+                form_data["phone"] = phone
+                form_data["regions"] = regions
+                form_data["min_magnitude"] = min_magnitude
 
                 if not validate_name(first_name, last_name):
                     st.warning("Please provide a valid first and last name.")
-                elif email and not validate_email(email):
+                elif (contact_preference in ["Email", "Both"] and not validate_email(email)):
                     st.warning("Please provide a valid email address.")
-                elif phone and not validate_phone_number(phone):
+                elif (contact_preference in ["Phone", "Both"] and not validate_phone_number(phone)):
                     st.warning(
-                        "Please provide a valid number, must start with 07 and be 10 or 11 digits in total.")
+                        "Please provide a valid number, must start with 07 and be 10 or 11 digits in total."
+                    )
                 elif not regions:
                     st.warning("Please select at least one region.")
                 else:
-                    st.session_state.form_data = {
-                        "first_name": first_name,
-                        "last_name": last_name,
-                        "email": email,
-                        "regions": regions,
-                        "min_magnitude": min_magnitude
-                    }
-                    # Check if user already exists, if they do, then give them a new topic subscription
-                    # If they do not exist, then add them to user table and give them the topic (Might have to mess with SNS here??)
                     st.success("Successfully subscribed!")
+
+                    st.session_state.form_data = {
+                        "first_name": "",
+                        "last_name": "",
+                        "email": "",
+                        "phone": "",
+                        "regions": [],
+                        "min_magnitude": None,
+                    }
 
     with right:
         st.write("")
@@ -107,6 +136,9 @@ def validate_name(first_name: str, last_name: str) -> bool:
 def validate_email(email: str) -> bool:
     """Validates the email against a regEx"""
 
+    if email is None:
+        return False
+
     email_pattern = r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
 
     return bool(re.match(email_pattern, email))
@@ -114,6 +146,9 @@ def validate_email(email: str) -> bool:
 
 def validate_phone_number(phone: str) -> bool:
     """Validates the phone number against a regEx"""
+
+    if phone is None:
+        return False
 
     phone_pattern = r"^07\d{8,9}$"
 
